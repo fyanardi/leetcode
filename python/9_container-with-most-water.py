227149feafd80b11b01e24b1bb34df304e5ca080
Fredy Yanardi <fyanardi@gmail.com>
2026-04-07 00:50:24 +0800

    Replace solution for problem 9 with more efficient O(n) algorithm

--- python/9_container-with-most-water.py
+++ python/9_container-with-most-water.py
@@ -3,22 +3,20 @@ class Solution:
         if len(height) <= 1:
             return 0
 
+        i = 0
+        j = len(height) - 1
+
         max_volume = 0
-        for i in range(len(height) - 1):
-            if height[i] == 0:
-                continue
 
-            # skip j where it is not possible to have volume greater than the current max volume
-            skip = 1
-            if height[i] < max_volume:
-                skip = -( -max_volume // height[i])
+        while i < j:
+            volume = (j - i) * min(height[i], height[j])
+            max_volume = max(max_volume, volume)
 
-            for j in range(i + skip, len(height)):
-                if height[j] == 0:
-                    continue
-
-                volume = min(height[i], height[j]) * (j - i)
-                max_volume = max(max_volume, volume)
+            # advance the position with lower height (lower height determines the volume)
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
 
         return max_volume
 
@@ -28,3 +26,4 @@ if __name__ == "__main__":
     assert solution.maxArea([1,8,6,2,5,4,8,3,7]) == 49
     assert solution.maxArea([1,1]) == 1
     assert solution.maxArea([1]) == 0
+    assert solution.maxArea([*list(range(0, 10001)), *list(range(9999, 0, -1))]) == 50000000
