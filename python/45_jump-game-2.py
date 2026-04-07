5e7c5c347836f09f11145ae4d73e2c9a84b685b1
Fredy Yanardi <fyanardi@gmail.com>
2026-04-07 23:59:45 +0800

    Use O(n) greedy algorithm for problem 45

--- python/45_jump-game-2.py
+++ python/45_jump-game-2.py
@@ -1,22 +1,23 @@
 class Solution:
     def jump(self, nums: list[int]) -> int:
-        l = len(nums)
-        max_int = 1 << 31 - 1
-        # min jumps at every position, initialized with max int except the last position which is
-        # the destination
-        min_jumps = [max_int for _ in range(l)]
-        min_jumps[l - 1] = 0
+        jumps = 0
+        # [l, r] is the window of possible positions of every jump, every position in the window
+        # needs to be considered to derive the next window
+        l = r = 0
 
-        for i in range(l - 2, -1, -1):
-            # at every position i, minimize the number of jumps by checking against jumping by j
-            # (1 <= j <= nums[i])
-            # to position i + j
-            for j in range(1, nums[i] + 1):
-                if i + j >= l:
-                    break
-                min_jumps[i] = min(1 + min_jumps[i + j], min_jumps[i])
+        while r < len(nums) - 1:
+            # within this window, what's the farthest we can go
+            farthest = 0
 
-        return min_jumps[0]
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+
+            # we've jumped and define next window to evaluate
+            jumps += 1
+            l = r + 1
+            r = farthest
+
+        return jumps
 
 
 if __name__ == "__main__":
