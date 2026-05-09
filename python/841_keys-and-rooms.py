6758a33971607c89bad054d632fe779606b4620c
Fredy Yanardi <fyanardi@gmail.com>
2026-05-09 12:12:46 +0800

    Problem 841: use a single integer to keep track of visited rooms

--- python/841_keys-and-rooms.py
+++ python/841_keys-and-rooms.py
@@ -1,17 +1,38 @@
 class Solution:
     def canVisitAllRooms(self, rooms: list[list[int]]) -> bool:
         # 2 <= n <= 1000, rooms[0] is guaranteed to be valid
+
+        # use array of boolean to keep track of visited rooms
+        #visited = [False] * len(rooms)
+        #keys = [0]
+
+        #while len(keys) > 0:
+        #    key = keys.pop()
+
+        #    if not visited[key]:
+        #        visited[key] = True
+        #        keys.extend(rooms[key])
+
+        #return len([v for v in visited if v is False]) == 0
+
+        # alternative version using bits to keep track of visited rooms
         visited = [False] * len(rooms)
+        all_visited = (1 << len(rooms)) - 1
+        visited = 0
         keys = [0]
 
         while len(keys) > 0:
             key = keys.pop()
+            mask = 1 << key
 
-            if not visited[key]:
-                visited[key] = True
+            if (visited & mask) == 0:
+                visited = visited | mask
                 keys.extend(rooms[key])
 
-        return len([v for v in visited if v is False]) == 0
+                if visited == all_visited:
+                    return True
+
+        return visited == all_visited
 
 
 if __name__ == "__main__":
