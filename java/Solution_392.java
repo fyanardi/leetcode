4804dbaeac9b70be97b1fe0576631a6787bd289b
Fredy Yanardi <fyanardi@gmail.com>
2026-05-21 18:29:06 +0800

    Accepted solution for problem 392 Is Subsequence

---
+++ java/Solution_392.java
@@ -0,0 +1,34 @@
+/**
+ * 392. Is Subsequence
+ */
+public class Solution_392 {
+    static class Solution {
+        public boolean isSubsequence(String s, String t) {
+            int i = 0;
+            int j = 0;
+
+            while (i < s.length() && j < t.length()) {
+                if (s.charAt(i) == t.charAt(j)) {
+                    if (i == s.length() - 1) {
+                        return true;
+                    }
+                    i++;
+                    j++;
+                }
+                else {
+                    j++;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        assert solution.isSubsequence("abc", "ahbgdc") == true;
+        assert solution.isSubsequence("axc", "ahbgdc") == false;
+        assert solution.isSubsequence("", "ahbgdc") == true;
+    }
+}
